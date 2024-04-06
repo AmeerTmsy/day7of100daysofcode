8d0573b15817d50adaf5d1452bea0b66ef68abb7
Ameer <tmsyameer@gmail.com>
2024-04-06 12:46:42 +0530

    first commit. created and filled data index.html, index.css, index.js, and readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# day7of100daysofcode
+
+
+[click here to see the result](https://ameertmsy.github.io/day7of100daysofcode/)
---
+++ index.js
@@ -0,0 +1,17 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const content = document.getElementById('content');
+
+btn1.addEventListener('click', () => {
+    content.classList.add("square");
+    content.style.backgroundColor = 'pink';
+    btn1.style.backgroundColor = "blue"
+    btn2.style.backgroundColor = "lightblue"
+})
+
+btn2.addEventListener('click', () => {
+    content.classList.remove("square");
+    content.style.backgroundColor = 'rebeccapurple'
+    btn2.style.backgroundColor = "blue"
+    btn1.style.backgroundColor = "lightblue"
+})
